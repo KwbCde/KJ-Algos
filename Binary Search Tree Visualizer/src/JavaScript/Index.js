4cad9debbeb1c707dd269b0e389d3cac4a04bd67
KwbCde <kwab822@gmail.com>
2025-02-16 17:22:56 -0500

    React Implemented
    Deleted old html files in favor of react.

--- Binary Search Tree Visualizer/src/JavaScript/Index.js
+++
@@ -1,8 +0,0 @@
-import {createRoot } from 'react-dom/client';
-
-//Clear existing html
-document.body.innerHTML = '<div id="app"></div>';
-
-//render react component
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
